2cc25e8ce58de8bd0e4184da4431b7fbb451f84c
Yusuke Yasuda <yusuke.007.yasud@gmail.com>
2016-10-17 22:53:46 +0900

    neuralnet_mnist.py

---
+++ common/functions.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+import numpy as np
+
+def identity_function(x):
+    return x
+
+def step_function(x):
+    return np.array(x > 0, dtype=np.int)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def relu(x):
+    return np.maximum(0, x)
+
+def softmax(x):
+    t = np.max(x)
+    xx = x - t
+    return np.exp(xx) / np.sum(np.exp(xx))
