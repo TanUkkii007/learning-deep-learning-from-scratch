fe6e0c17994cfd0c11aedeb1c4ed1e83ac039bdc
Yusuke Yasuda <yusuke.007.yasud@gmail.com>
2016-10-21 23:11:28 +0900

    gradient_2d.py

---
+++ ch04/gradient_2d.py
@@ -0,0 +1,62 @@
+# coding: utf-8
+
+import numpy as np
+import matplotlib.pylab as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def _numerical_gradient_no_batch(f, x):
+    print(x.ndim)
+    h = 1e-4
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        # f(x+h)
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        # f(x-h)
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+
+    return grad
+
+def numerical_gradient(f, X):
+    if X.ndim == 1:
+        return _numerical_gradient_no_batch(f, X)
+    else:
+        grad = np.zeros_like(X)
+
+        for idx, x in enumerate(X):
+            grad[idx] = _numerical_gradient_no_batch(f, x)
+
+        return grad
+
+def function_2(x):
+    if x.ndim == 1:
+        return np.sum(x**2)
+    else:
+        return np.sum(x**2, axis=1)
+
+if __name__ == '__main__':
+    x0 = np.arange(-2, 2.5, 0.25)
+    x1 = np.arange(-2, 2.5, 0.25)
+    X, Y = np.meshgrid(x0, x1)
+
+    X = X.flatten()
+    Y = Y.flatten()
+
+    grad = numerical_gradient(function_2, np.array([X, Y]))
+
+    plt.figure()
+    plt.quiver(X, Y, -grad[0], -grad[1], angles="xy", color="#666666")
+    plt.xlim([-2, 2])
+    plt.ylim([-2, 2])
+    plt.xlabel('x0')
+    plt.ylabel('x1')
+    plt.grid()
+    plt.legend()
+    plt.draw()
+    plt.show()
