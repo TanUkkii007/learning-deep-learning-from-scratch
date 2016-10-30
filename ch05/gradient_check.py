76dc4038a0b51ffaf0015c1402535ec7d7085401
Yusuke Yasuda <yusuke.007.yasud@gmail.com>
2016-10-31 00:02:32 +0900

    gradient_check.py

---
+++ ch05/gradient_check.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+import sys, os
+import numpy as np
+from dataset.mnist import load_mnist
+from ch05.two_layer_net import TwoLayerNet
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+
+x_batch = x_train[:3]
+t_batch = t_train[:3]
+
+grad_numerical = network.numerical_gradient(x_batch, t_batch)
+grad_backprop = network.gradient(x_batch, t_batch)
+
+for key in grad_numerical.keys():
+    diff = np.average(np.abs(grad_backprop[key] - grad_numerical[key]))
+    print(key + ":" + str(diff))
