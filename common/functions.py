9a2c5ef77e86712552747bab18b63929440d7e8f
Yusuke Yasuda <yusuke.007.yasud@gmail.com>
2016-10-30 17:29:13 +0900

    refine functions.py

--- common/functions.py
+++ common/functions.py
@@ -14,10 +14,23 @@ def relu(x):
     return np.maximum(0, x)
 
 def softmax(x):
+    if x.ndim == 2:
+        x = x.T
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T
     t = np.max(x)
     xx = x - t
     return np.exp(xx) / np.sum(np.exp(xx))
 
 def cross_entropy_error(y, t):
-    delta = 1e-7
-    return - np.sum(t * np.log(y + delta))
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    # convert one-hot-vector to answer-label-index vector
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+
+    batch_size = y.shape[0]
+    return - np.sum(np.log(y[np.arange(batch_size), t])) / batch_size
