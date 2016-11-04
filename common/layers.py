bfa134189eef3d6b16be563b4f1e9f2a8da79352
Yusuke Yasuda <yusuke.007.yasud@gmail.com>
2016-11-04 22:32:52 +0900

    batch_norm_test.py

--- common/layers.py
+++ common/layers.py
@@ -91,3 +91,87 @@ class SoftmaxWithLoss:
             dx = dx / batch_size
 
         return dx
+
+
+class BatchNormalization:
+    """
+    http://arxiv.org/abs/1502.03167
+    """
+    def __init__(self, gamma, beta, momentum=0.9, running_mean=None, running_var=None):
+        self.gamma = gamma
+        self.beta = beta
+        self.momentum = momentum
+        self.input_shape = None
+
+
+        self.running_mean = running_mean
+        self.running_var = running_var
+
+
+        self.batch_size = None
+        self.xc = None
+        self.std = None
+        self.dgamma = None
+        self.dbeta = None
+
+    def forward(self, x, train_flg=True):
+        self.input_shape = x.shape
+        if x.ndim != 2:
+            N, C, H, W = x.shape
+            x = x.transpose(1, 0, 2, 3).reshape(C, -1)
+
+        out = self.__forward(x, train_flg)
+
+        return out.reshape(*self.input_shape)
+
+    def __forward(self, x, train_flg):
+        if self.running_mean is None:
+            N, D = x.shape
+            self.running_mean = np.zeros(D)
+            self.running_var = np.zeros(D)
+
+        if train_flg:
+            mu = x.mean(axis=0)
+            xc = x - mu
+            var = np.mean(xc**2, axis=0)
+            std = np.sqrt(var + 10e-7)
+            xn = xc / std
+
+            self.batch_size = x.shape[0]
+            self.xc = xc
+            self.xn = xn
+            self.std = std
+            self.running_mean = self.momentum * self.running_mean + (1-self.momentum) * mu
+            self.running_var = self.momentum * self.running_var + (1-self.momentum) * var
+        else:
+            xc = x - self.running_mean
+            xn = xc / (np.sqrt(self.running_var + 1e-7))
+
+        out = self.gamma * xn + self.beta
+        return out
+
+    def backward(self, dout):
+        if dout.ndim != 2:
+            N, C, H, W = dout.shape
+            dout = dout.transpose(1, 0, 2, 3).reshape(C, -1)
+
+        dx = self.__backward(dout)
+
+        dx = dx.reshape(*self.input_shape)
+        return dx
+
+    def __backward(self, dout):
+        dbeta = dout.sum(axis=0)
+        dgamma = np.sum(self.xn * dout, axis=0)
+        dxn = self.gamma * dout
+        dxc = dxn / self.std
+        dstd = -np.sum((dxn * self.xc) / (self.std * self.std), axis=0)
+        dvar = 0.5 * dstd / self.std
+        dxc += (2.0 / self.batch_size) * self.xc * dvar
+        dmu = np.sum(dxc, axis=0)
+        dx = dxc - dmu / self.batch_size
+
+        self.dgamma = dgamma
+        self.dbeta = dbeta
+
+        return dx
---
+++ common/multi_layer_net_extend.py
@@ -0,0 +1,104 @@
+# coding: utf-8
+import numpy as np
+from collections import OrderedDict
+from common.layers import *
+from common.gradient import numerical_gradient
+
+
+class MultiLayerNetExtend:
+    def __init__(self, input_size, hidden_size_list, output_size,
+                 activation='relu', weight_init_std='relu', weight_decay_lambda=0,
+                 use_dropout=False, dropout_ratio=0.5, use_batchnorm=False):
+        self.input_size = input_size
+        self.output_size = output_size
+        self.hidden_size_list = hidden_size_list
+        self.hidden_layer_num = len(hidden_size_list)
+        self.use_dropout = use_dropout
+        self.weight_decay_lambda = weight_decay_lambda
+        self.use_batchnorm = use_batchnorm
+        self.params = {}
+
+        self.__init_weight(weight_init_std)
+
+        activation_layer = {'sigmoid': Sigmoid, 'relu': Relu}
+        self.layers = OrderedDict()
+        for idx in range(1, self.hidden_layer_num + 1):
+            self.layers['Affine' + str(idx)] = Affine(self.params['W' + str(idx)], self.params['b' + str(idx)])
+
+            if self.use_batchnorm:
+                self.params['gamma' + str(idx)] = np.ones(hidden_size_list[idx - 1])
+                self.params['beta' + str(idx)] = np.zeros(hidden_size_list[idx - 1])
+                self.layers['BatchNorm' + str(idx)] = BatchNormalization(self.params['gamma' + str(idx)], self.params['beta' + str(idx)])
+
+            self.layers['Activation_function' + str('idx')] = activation_layer[activation]()
+
+            if self.use_dropout:
+                # self.layers['Dropout' + str(idx)] = Dropout(dropout_ratio)
+                1
+
+        idx = self.hidden_layer_num + 1
+        self.layers['Affine' + str(idx)] = Affine(self.params['W' + str(idx)], self.params['b' + str(idx)])
+
+        self.last_layer = SoftmaxWithLoss()
+
+
+
+    def __init_weight(self, weight_init_std):
+        all_size_list = [self.input_size] + self.hidden_size_list + [self.output_size]
+        for idx in range(1, len(all_size_list)):
+            scale = weight_init_std
+            if str(weight_init_std).lower() in ('relu', 'he'):
+                scale = np.sqrt(2.0 / all_size_list[idx - 1])
+            elif str(weight_init_std).lower() in ('sigmoid', 'xavier'):
+                scale = np.sqrt(1.0 / all_size_list[idx - 1])
+            self.params['W' + str(idx)] = scale * np.random.randn(all_size_list[idx - 1], all_size_list[idx])
+            self.params['b' + str(idx)] = np.zeros(all_size_list[idx])
+
+    def predict(self, x, train_flg=False):
+        for key, layer in self.layers.items():
+            if "Dropout" in key or "BatchNorm" in key:
+                x = layer.forward(x, train_flg)
+            else:
+                x = layer.forward(x)
+
+        return x
+
+    def loss(self, x, t, train_flg=False):
+        y = self.predict(x, train_flg)
+
+        weight_decay = 0
+        for idx in range(1, self.hidden_layer_num + 2):
+            W = self.params['W' + str(idx)]
+            weight_decay += 0.5 * self.weight_decay_lambda * np.sum(W**2)
+
+            return self.last_layer.forward(y, t) + weight_decay
+
+    def accuracy(self, X, T):
+        Y = self.predict(X, train_flg=False)
+        Y = np.argmax(Y, axis=1)
+        if T.ndim != 1 : T = np.argmax(T, axis=1)
+
+        accuracy = np.sum(Y == T) / float(X.shape[0])
+        return accuracy
+
+    def gradient(self, x, t):
+        self.loss(x, t, train_flg=True)
+
+        dout = 1
+        dout = self.last_layer.backward(dout)
+
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+
+        grads = {}
+        for idx in range(1, self.hidden_layer_num+2):
+            grads['W' + str(idx)] = self.layers['Affine' + str(idx)].dW + self.weight_decay_lambda * self.params['W' + str(idx)]
+            grads['b' + str(idx)] = self.layers['Affine' + str(idx)].db
+
+            if self.use_batchnorm and idx != self.hidden_layer_num + 1:
+                grads['gamma' + str(idx)] = self.layers['BatchNorm' + str(idx)].dgamma
+                grads['beta' + str(idx)] = self.layers['BatchNorm' + str(idx)].dbeta
+
+        return grads
