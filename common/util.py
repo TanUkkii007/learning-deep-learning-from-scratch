b4ea2a314c36aa1e5bcc969fe4eaee83d478fb23
Yusuke Yasuda <yusuke.007.yasud@gmail.com>
2016-11-05 17:50:38 +0900

    hyperparameter_optimization.py

--- common/util.py
+++ common/util.py
@@ -8,3 +8,11 @@ def smooth_curve(x):
     w = np.kaiser(window_len, 2)
     y = np.convolve(w/w.sum(), s, mode='valid')
     return y[5:len(y)-5]
+
+
+def shuffle_dataset(x, t):
+    permutation = np.random.permutation(x.shape[0])
+    x = x[permutation,:] if x.ndim == 2 else x[permutation,:,:,:]
+    t = t[permutation]
+
+    return x, t
